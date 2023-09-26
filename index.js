52aa9d7bafcf407c7d47a109176cc343e6f32476
workerandi <workerandiramirez@gmail.com>
2023-09-26 17:31:28 -0500

    Started JS
    -added the newCity function (selecting and changing new City)

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-let apiURL = "";
-let apiKey = "";
+function updatedPage(response) {}
+
+function newCity() {
+  let city = document.querySelector("#searchBar").value;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiKey = "of4be206ff10d3a71a40t7bf74fdc933";
+  axios.get(apiUrl).then(updatedPage);
+}
